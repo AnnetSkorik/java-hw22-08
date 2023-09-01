718c94c08036f2fb41332689bd94ee4487728cbe
AnnetSkorik <139224720+AnnetSkorik@users.noreply.github.com>
2023-09-01 11:43:02 +0200

    Add files via upload
    Здравствуйте!
    Я не совсем поняла задание 2

---
+++ homework2208.java
@@ -0,0 +1,56 @@
+public class homework2208 {
+    public static void main(String[] args) {
+
+        char myChar = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+        float myFloat = 4.7333436F;
+        double myDouble = 4.355453532;
+        long myLong = 12121;
+
+        int myInt1  = 345;
+
+        System.out.println("Character: " + myChar);
+        System.out.println("Integer: " + myInt);
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Float: " + myFloat);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Long: " + myLong);
+
+        System.out.println(" 3, 4, 5 -> Число" + myInt1);
+    }
+}
+
+
+    //Задание 1.
+//
+//
+//В методе main инициализировать все примитивные типы и не примитивные типы.
+//
+//Вывести их результат в консоль:
+//char: G
+//int: 89
+//byte: 4
+//short: 56
+//float: 4.7333436
+//double: 4.355453532
+//long: 12121
+//
+//
+//Character: G
+//Integer: 89
+//Byte: 4
+//Short: 56
+//Float: 4.7333436
+//Double: 4.355453532
+//Long: 12121
+//
+//**Задание 2.
+//
+//
+//Дано трехзначное число. Вывести на экран все цифры этого числа
+//Пример: 345
+//Вывод в консоль: Число 345 -> 3, 4, 5
+
